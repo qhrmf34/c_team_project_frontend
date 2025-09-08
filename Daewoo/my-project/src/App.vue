f3c2c7689f827560cca506f68f2a45f883e9325e
qhrmf34 <qhrmf34@naver.com>
2025-09-08 17:14:46 +0900

    hotel 폴더 작성 퍼블 각각 폴더만들고 작업

---
+++ Daewoo/my-project/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div v-for="item in list" :key="item">
+        {{item.title}}
+    </div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data(){
+    return{
+      list:[]
+    }
+  },  
+
+  async mounted(){
+     const result = await axios.get('https://jsonplaceholder.typicode.com/posts')
+     console.log(result.data);
+     this.list=result.data;
+    }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
