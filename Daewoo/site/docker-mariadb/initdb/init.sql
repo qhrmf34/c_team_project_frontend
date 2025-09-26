3d4b9da720ff26e1bf503ab8619481229f1fea85
qhrmf34 <qhrmf34@naver.com>
2025-09-26 11:55:57 +0900

    style. daewoo폴더 삭제

--- Daewoo/site/docker-mariadb/initdb/init.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL
-);
-
-INSERT INTO users (username, email) VALUES
-('tom', 'tom@example.com'),
-('jane', 'jane@example.com');
