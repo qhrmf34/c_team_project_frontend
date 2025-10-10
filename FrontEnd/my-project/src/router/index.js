179668e73c3f0a49ba7c218852c4eee13bbe136d
qhrmf34 <qhrmf34@naver.com>
2025-10-10 19:39:56 +0900

    feat. 화면별 title 추가

--- FrontEnd/my-project/src/router/index.js
+++ FrontEnd/my-project/src/router/index.js
@@ -19,67 +19,80 @@ const routes = [
   { 
     path: '/', 
     name: 'Home',
-    component: HotelMaster 
+    component: HotelMaster, 
+    meta: { title: '개발용' }
   },
    { 
     path: '/hoteltest', 
     name: 'HotelTest',
-    component: HotelTest 
+    component: HotelTest, 
+    meta: { title: '테스트' } 
   },
   { 
     path: '/hotelone', 
     name: 'HotelOne',
-    component: HotelOne 
+    component: HotelOne,
+    meta: { title: '메인' }
   },
     { 
     path: '/hoteltwo', 
     name: 'HotelTwo',
-    component: HotelTwo 
+    component: HotelTwo, 
+    meta: { title: '호텔' } 
   },
     { 
     path: '/hotelthree', 
     name: 'HotelThree',
-    component: HotelThree 
+    component: HotelThree, 
+    meta: { title: '객실' } 
   },
     { 
     path: '/hotelfour', 
     name: 'HotelFour',
-    component: HotelFour 
+    component: HotelFour, 
+    meta: { title: '결제' } 
   },
     { 
     path: '/hotelfive', 
     name: 'HotelFive',
-    component: HotelFive 
+    component: HotelFive, 
+    meta: { title: '티켓' } 
   },
     { 
     path: '/hotelsix', 
     name: 'HotelSix',
-    component: HotelSix 
+    component: HotelSix, 
+    meta: { title: '찜목록' } 
   },
     { 
     path: '/hotelaccount', 
     name: 'HotelAccount',
-    component: HotelAccount 
+    component: HotelAccount, 
+    meta: { title: '계정' } 
   },
     { 
     path: '/login', 
     name: 'HotelLogin',
-    component: HotelLogin 
+    component: HotelLogin, 
+    meta: { title: '로그인' } 
   },
     { 
     path: '/signup', 
     name: 'HotelSignup',
-    component: HotelSignup
+    component: HotelSignup, 
+    meta: { title: '회원가입' }
   },
   {
     path: '/auth/callback',
     name: 'AuthCallback',
-    component: AuthCallback
+    component: AuthCallback, 
+    meta: { title: '로딩' }
   },
   {
     path: '/admin',
     name: 'HotelAdmin',
-    component: HotelAdmin
+    component: HotelAdmin, 
+    meta: { title: '관리자 페이지' }
   }
 ]
 
@@ -87,5 +100,8 @@ const router = createRouter({
   history: createWebHistory(),
   routes
 })
-
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title || '기본 제목';
+  next();
+});
 export default router
