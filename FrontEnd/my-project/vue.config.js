8f218ea2b36b0d26e22b7ac005c3cc6bb5f1a1d7
qhrmf34 <qhrmf34@naver.com>
2025-10-15 21:26:23 +0900

    feat. 로그인,회원가입 로봇인증 추가

--- FrontEnd/my-project/vue.config.js
+++ FrontEnd/my-project/vue.config.js
@@ -1,4 +1,21 @@
+// vue.config.js
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  
+  // 개발 서버 설정
+  devServer: {
+    port: 8080,
+    hot: true,
+    // HMR 관련 설정
+    client: {
+      overlay: {
+        errors: true,
+        warnings: false,
+        runtimeErrors: false, // 런타임 에러 오버레이 비활성화
+      },
+    },
+  },
+
 })
